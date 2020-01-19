f125310a9f0b5f153b9dc1b45d031c378df7983c
Jeetendra Gan <jeetendra.gan@gmail.com>
2020-01-19 16:47:58 -0500

    Initial fetcher written

---
+++ tweepy_streamer.py
@@ -0,0 +1,26 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import credentials
+import json
+
+print(dir(StreamListener))
+
+class StdOutLinstener(StreamListener):
+    def on_data(self, data):
+        data_json = json.loads(data)
+        print("___________________")
+        print(data_json['text'])
+        return True        
+    
+    def on_error(self, status):
+        print(status)
+        return True
+
+if __name__ == '__main__':
+    listener = StdOutLinstener()
+    auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, listener)
+    stream.filter(track = ['Trump'])
